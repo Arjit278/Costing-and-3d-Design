d151b86a2c5ce609e215ac273236173094396fae
Arjit278 <arjit278@gmail.com>
2025-11-15 17:56:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 MODELS = {
     "FLUX.1-dev (Lineart / Mechanical)": "black-forest-labs/FLUX.1-dev",
     "Stable Diffusion XL (2D CNC Blueprints)": "stabilityai/stable-diffusion-xl-base-1.0",
-    "Stable Diffusion 3 Medium (3D Render)": "stabilityai/stable-diffusion-3-medium"
+    "Stable Diffusion 3 Medium (3D Render)": "stabilityai/stable-diffusion-3-medium-diffusers"
 }
 
 # -------------------------------
